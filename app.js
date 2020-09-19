a34cac2946fdc3dd6cf53485e20e312905559717
Chester <chesterdeveloper2000@gmail.com>
2020-09-19 15:55:33 +0800

    making it dynamic -deleted template html - recreated the html in js using the updateDOM (lines 14 - 52) - appended it in the dom (line 56)

--- app.js
+++ app.js
@@ -1,10 +1,62 @@
-const hearts = document.querySelectorAll(".like");
+// const hearts = document.querySelectorAll(".like");
+const quoteContainer = document.querySelector(".quote-container");
+let quotesArray;
 
-hearts.forEach((heart) => {
-    heart.addEventListener("click", () => {
-        heart.classList.toggle("like-blast");
+// hearts.forEach((heart) => {
+//     heart.addEventListener("click", () => {
+//         heart.classList.toggle("like-blast");
+//     });
+// });
+
+const updateDOM = () => {
+    quotesArray.forEach((quote) => {
+        // quote card
+        const card = document.createElement("div");
+        card.classList.add("quote");
+
+        // ********************************************************
+        // quote text==============================================
+        const quoteTextContainer = document.createElement("div");
+        quoteTextContainer.classList.add("text");
+
+        const quoteText = document.createElement("p");
+        quoteText.textContent = quote.q;
+        // ********************************************************
+
+        // ********************************************************
+        // author  ================================================
+        const authorLike = document.createElement("div");
+        authorLike.classList.add("author-like");
+
+        const author = document.createElement("p");
+        author.textContent= quote.a;
+
+        // ********************************************************
+
+        // ********************************************************
+        // like ❤  ===============================================
+        const likeContainer = document.createElement("div");
+        likeContainer.classList.add("like-container");
+
+        const like = document.createElement("div");
+        like.classList.add("like");
+        // ********************************************************
+
+        // ********************************************************
+        // appending every createElement  =========================
+        likeContainer.appendChild(like);
+        authorLike.append(author, likeContainer);
+
+        quoteTextContainer.appendChild(quoteText) 
+
+        card.append(quoteTextContainer, authorLike)
+
+        // console.log(card)
+        // console.log(quoteContainer)
+        quoteContainer.appendChild(card)
+        // ********************************************************
     });
-});
+};
 
 // get quotes
 const getQuote = async () => {
@@ -12,8 +64,10 @@ const getQuote = async () => {
     const apiUrl = "https://zenquotes.io/api/quotes";
     try {
         const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        console.log(data);
+        quotesArray = await response.json();
+        console.log(quotesArray[1].q);
+        console.log(quotesArray[1].a);
+        updateDOM()
     } catch (error) {
         console.log(error);
     }
