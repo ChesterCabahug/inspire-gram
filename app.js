5d31eb5275cfae69dff3c3e8e0c74252797ff659
Chester <chesterdeveloper2000@gmail.com>
2020-09-19 11:59:07 +0800

    yey, was able to fetch quote data

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const heart = document.querySelector(".like");
+const hearts = document.querySelectorAll(".like");
 
-heart.addEventListener("click", () => {
-    heart.classList.toggle("like-blast")
+hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+        heart.classList.toggle("like-blast");
+    });
 });
+
+// get quotes
+const getQuote = async () => {
+    const proxyUrl = "https://dry-hamlet-95494.herokuapp.com/";
+    const apiUrl = "https://zenquotes.io/api/quotes";
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getQuote();
